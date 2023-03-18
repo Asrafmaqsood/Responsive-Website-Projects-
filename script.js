f7276e4c1afeb524bf8f88fea46ea42a348fbc53
Asrafmaqsood <107093240+Asrafmaqsood@users.noreply.github.com>
2023-03-18 23:29:32 +0530

    Add files via upload
    This project is my first fully reponsive web project using HTML CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.food-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: ".prev-btn",
+        nextArrow: ".next-btn",
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive:
+            [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+    });
+    $(".nav-trigger").click(function () {
+        $(".site-content-wrapper").toggleClass('scaled');
+    })
+});
